85d1d0b9d2a5bc1e561777b6083f83847e43933e
Juliano ChaChaves <chavesjuliano7@gmail.com>
2020-04-27 00:31:18 -0300

    first commit

---
+++ 7-condicionais.js
@@ -0,0 +1,15 @@
+console.log(`Trabalhando com condicionais`);
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1);
+
+console.log(listaDeDestinos);
+
